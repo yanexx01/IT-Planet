919a5f0581addf7d0db1caeff64193d21eee2984
Alex <as_alexsandrov@mail.ru>
2026-03-20 18:23:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+
+## Сборка и запуск 
+
+### Требования
+
+-   [Flutter SDK](https://docs.flutter.dev/get-started/install) (версия 3.0 или выше)
+-   Dart SDK (входит в состав Flutter)
+-   IDE: [VS Code](https://code.visualstudio.com/) или [Android Studio](https://developer.android.com/studio)
+-   Эмулятор (Android/iOS) или физическое устройство
+
+
+---
+
+### 1. Установка зависимостей
+
+Установите все необходимые пакеты:
+
+
+    flutter pub get
+
+### 2. Генерация кода базы данных 
+
+Проект использует Hive для хранения данных. Для работы моделей необходимо сгенерировать адаптеры (`*.g.dart` файлы).
+
+Запустите команду build_runner:
+
+
+    dart run build_runner build --delete-conflicting-outputs
+
+_Если вы используете старую версию Flutter/Dart, команда может быть: `flutter pub run build_runner build --delete-conflicting-outputs`_
+
+После успешного выполнения в папке `lib/models/` должен появиться файл `entry.g.dart`.
+
+### 3.1 Запуск приложения
+
+Подключите устройство или запустите эмулятор, затем выполните:
+
+    flutter run
+
+
+### 3.2 Сборка релизной версии
+
+Перейдите в папку с Java и создайте ключ:
+
+    keytool -genkey -v -keystore %userprofile%\upload-keystore.jks -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias upload
+
+**Важно:** У вас должна быть установлена Java и библиотека `keytool` (идёт в комплекте с Java).
+
+Придумайте сложный пароль и не потеряйте сгенерированный файл
+
+Измените файл `android/key.properties` следующим образом:
+
+    storePassword=<Ваш пароль store password>
+    keyPassword=<Ваш пароль от ключа>
+    keyAlias=upload
+    storeFile=<Полный путь до директории где лежит сгенерированный ключ>
+
+Соберите приложение:
+
+    flutter build apk --release
+
+Файл APK будет находиться по пути: `build/app/outputs/flutter-apk/app-release.apk`
---
+++ android/app/build.gradle.kts
@@ -0,0 +1,61 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    println("Warning: key.properties not found!")
+}
+android {
+    namespace = "com.example.bruh"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.bruh"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            val storeFilePath = keystoreProperties["storeFile"] as String
+            storeFile = file(storeFilePath)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
